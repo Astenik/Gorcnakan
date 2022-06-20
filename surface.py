ac900db69ad349dfd0687fe05af70751ad312c6a
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-20 23:13:23 +0400

    Create surface.py

---
+++ surface.py
@@ -0,0 +1,7 @@
+def area(length:float, width:float):
+        """This function returns the area of room."""
+        return length * width
+ 
+length = float(input("insert the length of your room: ")
+width = float(input("insert the width of your room: ")
+print(f'the area of your room is: {area(length, width)}')
