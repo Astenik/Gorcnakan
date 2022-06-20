44024bd7bbd9119faab47b0c1dcdfa9c503e8816
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-20 23:21:46 +0400

    Update surface.py

--- surface.py
+++ surface.py
@@ -1,7 +1,13 @@
 def area(length:float, width:float):
-        """This function returns the area of room."""
+        """This function returns the area of surface."""
         return length * width
  
 length = float(input("insert the length of your room: ")
 width = float(input("insert the width of your room: ")
 print(f'the area of your room is: {area(length, width):f2}')
+
+              
+l = float(input("insert the length of your plot: ")
+w = float(input("insert the width of your plot: ")
+print(f'the area of your plot is: {(area(l, w) / 43560):f2} akra')
+          
