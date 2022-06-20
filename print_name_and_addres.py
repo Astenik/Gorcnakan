6725536002d88581e314ace6d015f128b75f1c36
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-20 22:59:58 +0400

    Create print_name_and_addres.py

---
+++ print_name_and_addres.py
@@ -0,0 +1,6 @@
+def name_address(anun):
+      return anun
+      
+name = input("insert your name: ")
+address = input("insert your addres: ")
+print(f'my name is {name_address(name)} and my addres is {name_address(address)}')
