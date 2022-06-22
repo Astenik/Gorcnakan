df714f3f1a73c4f5895a0d3647774ce8ac697bd8
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-22 18:08:05 +0400

    Create polindrom.py

---
+++ polindrom.py
@@ -0,0 +1,14 @@
+def is_polindrom(n):
+        """this function checs is your sentence polindrom or not."""
+        list = [None] * len(n)
+        i = 0
+        j = len(n) - 1 
+        while i < len(n):
+             list[i] = n[j]
+             i += 1 
+             j -= 1 
+        return list == n 
+        
+sentenses = input("insert the sentence: ")
+print(f'is tour sentence polindrom? {is_polindrom(sentenses)}')
+    
