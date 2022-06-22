748ce74eb183728050d508e315f2254cb5a0bed6
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-22 21:32:40 +0400

    Create souv_trif.py

---
+++ souv_trif.py
@@ -0,0 +1,5 @@
+souvenir =  int(input("insert the count of your souvinirs: "))
+trifle =  int(input("insert the number of your trifles: "))
+print(f'the  weight of your souvenirs and trifles is: {souvenir*75 + trifle*112}') 
+
+      
