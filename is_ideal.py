10e40dcb4100c8fd8c8dba3b2853a53dcf367ec3
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-22 15:50:35 +0400

    Update is_ideal.py

--- is_ideal.py
+++ is_ideal.py
@@ -1,4 +1,5 @@
 def is_ideal(num:int):
+    """This function returns true if the number is perfect."""
     i = 1 
     sum = 0
     while i <= num // 2:
@@ -8,3 +9,11 @@ def is_ideal(num:int):
 
 n = int(input("insert number: "))
 print(is_ideal(n))
+
+
+def is_perfect(n):
+     """This function returns true if the number is perfect."""
+     return sum([i for i in range(1, n) if n % i ==0]) == n
+
+m = int(input("insert number: "))
+print(is_perfect(m))
