b02b69d8bde904e72e7d802553cbf932748b5b2c
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-22 15:54:51 +0400

    Update div.py

--- div.py
+++ div.py
@@ -1,4 +1,5 @@
 def divisiors(num:int):
+        """This function returns list consists of number's divisiors."""
         i = 1
         div = []
         while i <= num:
@@ -9,3 +10,11 @@ def divisiors(num:int):
             
 n = int(input("insert number"))
 print(f'divisors of your number are: {divisiors(n)}')
+
+
+def divisior(n):
+      """This function returns list consists of number's divisiors."""
+      return [i for i in range(1, n) if n % i ==0] == n
+
+m = int(input("insert number"))
+print(divisior(m))
