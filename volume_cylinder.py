586d70b862acf577a65bd618db5f22b50587fea1
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-23 10:35:29 +0400

    Create volume_cylinder.py

---
+++ volume_cylinder.py
@@ -0,0 +1,5 @@
+pi = 3.14
+rad = float(input("insert the radius of your cylinder: "))
+high = float(input("insert the high of your cylinder: "))
+cylinder_volume = pi * rad**2 * high
+print(f'volume of your cylinder is equal to: {cylinder_volume:f2}')
