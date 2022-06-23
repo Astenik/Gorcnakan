c86ab5a13dba8f1da6c1f7bd17294aed9513d671
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-23 10:27:28 +0400

    Create area_volume.py

---
+++ area_volume.py
@@ -0,0 +1,6 @@
+pi = 3.14
+r = float(input("insert the radius: "))
+area = pi * r**2
+volume = (4 * pi * r**3) / 3
+print(f'the area of your circle is equal to: {area}')
+print(f'the volume of your ball is equal to: {volume}')
