51c7dfc4711f489333ae0364b13d091664149df8
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-23 20:44:08 +0400

    Update list_proj.py

--- list_proj.py
+++ list_proj.py
@@ -9,7 +9,8 @@ for ind in range(len(lst)):
          for ind1 in range(ind + 1, len(lst)):
               if lst[ind] == lst[ind1]:
                        lst.remove(lst[ind1])
-                       ind += 1 
-                       ind1 += 1 
+          
+
 
 print(f'your names list is: {lst}') 
+
