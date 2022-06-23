171742219fab442b627c4df184b273e4e6c1e529
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-23 10:05:08 +0400

    Create distance.py

---
+++ distance.py
@@ -0,0 +1,4 @@
+distance = float(input("insert  the distance with funts: "))
+print(f'the distance with duyms is equal to: {12 * distance} duym')
+print(f'the distance with  yards is equal to: {distance / 3} yard')
+print(f'the distance with miles is equal to: {distance / 5280} mile')
