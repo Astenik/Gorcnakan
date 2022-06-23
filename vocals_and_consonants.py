33d7eec65f81abb8f0a4868032015f5194cdd4b7
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-23 12:39:46 +0400

    Create vocals_and_consonants.py

---
+++ vocals_and_consonants.py
@@ -0,0 +1,9 @@
+letter = input("insert the letter: ")
+if letter == 'y':
+     print("your lettre can be both vocal and consonant")
+
+elif letter in ('a', 'i', 'o', 'e', 'u'):
+     print("your lettre is vocal")
+
+else:
+      print("your lettre is consonant")
