24e0766eae035daa77b381963f6d0a4cc1c47b76
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-23 19:42:42 +0400

    Update list_proj.py

--- list_proj.py
+++ list_proj.py
@@ -5,12 +5,11 @@ while lst[i] != '':
       lst.append(input("insert name: "))
       i += 1
 
-ind = 0
-ind1 = 1
-while ind < len(lst):
-          if lst[ind] == lst[ind1]:
-               lst.remove(lst[ind1])
-          ind += 1 
-          ind1 += 1 
+for ind in range(len(lst)):
+         for ind1 in range(1, len(lst)):
+              if lst[ind] == lst[ind1]:
+                       lst.remove(lst[ind1])
+                       ind += 1 
+                       ind1 += 1 
 
 print(f'your names list is: {lst}') 
