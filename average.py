542aa76ad19ac26e81a65b286ba6a83c6e4879c6
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-23 19:28:52 +0400

    Update average.py

--- average.py
+++ average.py
@@ -1,11 +1,20 @@
 def average(lst):
-     """this function counts your list average."""
-      return sum(lst) / len(lst)
+       """this function counts your list average."""
+       return sum(lst) / len(lst)
     
 
 data = [90, 6, 5, 8]
-nums = list(input("insert the list: "))
+nums = []
+nums.append(int(input("insert the list: ")))
+i = 0
+while nums[i] != 0:
+      nums.append(int(input("insert the list: ")))
+      i += 1
+
 print(f'the average of your lis is: {average(data)}')
 print(f'the smaller numbers from average are: {[i for i in range(len(data)) if i < average(data)]}')
 print(f'the equal numbers to average are: {[i for i in range(len(data)) if i == average(data)]}')
 print(f'the larger numbers from average are: {[i for i in range(len(data)) if i > average(data)]}')
+
+
+
