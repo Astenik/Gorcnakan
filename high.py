4dc7986e6882c2062b95de9c8a3f870f22364926
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-23 09:40:10 +0400

    Create high.py

---
+++ high.py
@@ -0,0 +1,5 @@
+high = float(input("insert your high with funts: "))
+print(f'your high with santimetres is: {12 * 2.54 * high} santimetre')
+
+high_duym = float(input("insert your high with duyms:"))
+print(f'your high with santimetres is: {2.54 * high_duym} santimetre')
